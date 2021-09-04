1e83aeb5e1238a8d874c1816b50b8a91a29b0eaf
abi0116 <77713619+abi0116@users.noreply.github.com>
2021-09-04 11:25:53 +0900

    19 NULLのレコード抽出

---
+++ main19.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (
+  id INT NOT NULL AUTO_INCREMENT,
+  message VARCHAR(140),
+  likes INT,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO posts (message, likes) VALUES
+  ('Thanks', 12),
+  ('Arigato', 4),
+  ('Merci', NULL),
+  ('Gracias', 15),
+  ('Danke', NULL);
+
+-- SELECT * FROM posts WHERE likes != 12; NULLレコードなしの12以外
+-- SELECT * FROM posts WHERE likes != 12 OR likes IS NULL; NULLレコードありの12以外
+SELECT * FROM posts WHERE likes IS NOT NULL; --NULL以外のレコード
